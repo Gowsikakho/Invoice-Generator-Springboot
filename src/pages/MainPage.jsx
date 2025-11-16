0248b52d81d18be132a1b03217696609ac5ae50b
Gowsikakho <arulgowsi3357@gmail.com>
2025-11-16 22:05:33 +0530

    setting the template page

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,6 +1,29 @@
 const MainPage = () => {
     return (
-        <div>Main Page</div>
+        <div className="container-fluid bg-light min-vh-100 py-4">
+            <div className="container">
+                <div className="bg-white border rounded shadow-sm p-3 mb-4">
+                    <div className="d-flex align-items-center">
+                        Editable text box for title
+                    </div>
+                </div>
+                {/* Invoice form and template grid */}
+                <div className="row g-4 align-items stretch">
+                    {/* Invoice Form Section */}
+                        <div className="col-12 col-lg-6 d-flex">
+                            <div className="bg-white border rounded shadow-sm p-4 w-100">
+                                Invoice Form
+                            </div>
+                        </div>
+                    {/* Invoice Template Section */}
+                        <div className="col-12 col-lg-6 d-flex">
+                            <div className="bg-white border rounded shadow-sm p-4 w-100">
+                                Template Grid
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </div>
     )
 }
 export default MainPage;
